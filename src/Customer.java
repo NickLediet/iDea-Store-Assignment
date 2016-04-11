77e7d388844a54f5cffbdac304d509bd2a843989
Nick Lediet <nicklediet@gmail.com>
2016-04-11 15:22:00 -0400

    Customer and Global classes added and started

---
+++ src/Customer.java
@@ -0,0 +1,16 @@
+/* 
+ * Documentation to come
+ */
+public class Customer
+{	
+	/* instance fields */
+	private String firstName, lastName, address, province, postalCode,
+					email, paymentMethod;
+	private static final double HST_RATE = 0.13, SHIPPING_FEE = 25;
+	
+	/* Constructor */
+	public Customer(){
+		g
+	}
+}
+//End of class
---
+++ src/Global.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *  Program Name: Global.java
+		Purpose: A global object for customer transactions constructed via the customer constructor 
+		Coder: Nick Lediet 
+		Date: Apr 11, 2016
+ */
+import java.util.ArrayList;
+
+public class Global
+{
+	public static double subtotal;
+	public static Customer customer;
+	public static ArrayList<Purchase> cart;
+}
